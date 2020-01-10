fe035e331560c1d5d1392154b4ec20bf9c274e9d
niks255 <niks255292@gmail.com>
2020-01-10 22:31:52 +0300

    Unlock pro features

--- app/src/main/java/eu/faircode/email/ActivityBilling.java
+++ app/src/main/java/eu/faircode/email/ActivityBilling.java
@@ -191,7 +191,7 @@ public class ActivityBilling extends ActivityBase implements PurchasesUpdatedLis
     static boolean isPro(Context context) {
         if (BuildConfig.DEBUG && false)
             return true;
-        return PreferenceManager.getDefaultSharedPreferences(context).getBoolean("pro", false);
+        return true;
     }
 
     private BroadcastReceiver receiver = new BroadcastReceiver() {
